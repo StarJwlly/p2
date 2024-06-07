4426b836e0d1318f9ba931665bd8e69e4b000bbb
StarJwlly <fuukuoo@gmail.com>
2024-06-07 18:15:03 -0300

    feat(p2-pdmt): cria classe Img
    cria a classe Img e adiciona a visualizacao da imagem recebida pela API

--- App.tsx
+++ App.tsx
@@ -1,21 +1,51 @@
 import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import {GestureResponderEvent, Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {GestureResponderEvent, Pressable, StyleSheet, Text, View, Image } from 'react-native';
 
+class Img {
+  height: number;
+  width: number;
+  url: string;
+
+  constructor(url, width, height) {
+    this.url = url;
+    this.width = width;
+    this.height = height;
+  }
+}
 
 export default function App() {
 
+  const [images, setimages] = useState <Img[]>([]);
+
   function onPressFunction(event: GestureResponderEvent): void {
-    axios.get("https://api.thecatapi.com/v1/images/search?limit=5").then(v => {})
+    axios.get("https://api.thecatapi.com/v1/images/search?limit=5").then(v => {
+      let t = []
+      for (let i = 0; i < 5; i++) {
+        t.push(new Img (v.data[i].url, v.data[i].width, v.data[i].height));
+      }
+      console.log(v)
+      setimages(t);
+    })
   }
 
   return (
     <View style={styles.container}>
-      <Pressable onPress={onPressFunction}>
+      <Pressable 
+        onPress={onPressFunction}
+        style={styles.button}>
         <Text>I'm pressable!</Text>
       </Pressable>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        style={{
+          width: images[0] == undefined ? 0 : images[0].width,
+          height: images[0] == undefined ? 0 : images[0].height,
+        }}
+        source={{
+          uri: images[0] == undefined ? '' : images[0].url,
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -26,6 +56,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  button: {
+    width: '80%',
+    backgroundColor: '#0096F3',
+    padding: 12,
+    borderRadius: 4,
+    marginBottom: 12
   },
 });
